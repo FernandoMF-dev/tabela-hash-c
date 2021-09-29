fe61c59e425cd444d538c775da4a1d779ba82808
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2021-09-29 07:52:20 -0300

    Criando projeto para a implementação da hash fechada

---
+++ hash-fechada/cmake-build-debug/CMakeFiles/hash_fechada.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hash_fechada.dir/main.c.o"
+  "hash_fechada"
+  "hash_fechada.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hash_fechada.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

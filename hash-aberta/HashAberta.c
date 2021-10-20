8a6745039b0b3e69116493d4014ecc728efbd6c4
FernandoMF-dev <81381205+fernandomf-dev@users.noreply.github.com>
2021-10-20 08:21:25 -0300

    Implementando função para inserir novo elemento na Hash

--- hash-aberta/HashAberta.c
+++ hash-aberta/HashAberta.c
@@ -5,6 +5,7 @@
 #define ERROR_FALHA_ALOCACAO "\n\tERRO: Erro durante alocação de memória!\n"
 #define ERROR_INVALID_LENGTH "\n\tERRO: Não e possível criar uma HASH com menos de 1 índice!\n"
 
+#define EXPANSAO_MULTIPLICADOR 2
 #define STATUS_APAGADO -1
 #define STATUS_VAZIO 0
 #define STATUS_OCUPADO 1
@@ -16,6 +17,8 @@ int hashFunction(HashAberta *hash, char *key);
 Node *newNode(int index);
 
 Node *createNodeSequence(int length);
+
+HashAberta *expandsHash(HashAberta *base);
 
 // =-=-=-=-= METODOS PRIVADOS | IMPLEMENTAÇÃO =-=-=-=-=
 
@@ -52,16 +55,54 @@ int hashFunction(HashAberta *hash, char *key) {
     return keyIntValue % hash->length;
 }
 
+HashAberta *expandsHash(HashAberta *base) {
+    HashAberta *hash = newHashAberta(base->label, hash->length * EXPANSAO_MULTIPLICADOR, hash->chargeFactor);
+    Node *node = base->node;
+
+    do {
+        if (node->status == STATUS_OCUPADO) {
+            insertHash(hash, node->value);
+        }
+        node = node->next;
+    } while (node->index != 0);
+
+    free(base->node);
+    free(base);
+    return hash;
+}
+
 // =-=-=-=-= METODOS PUBLICOS =-=-=-=-=
 
 HashAberta *newHashAberta(char *label, int length, float chargeFactor) {
-    HashAberta *hash = (HashAberta*) malloc(sizeof(HashAberta));
+    HashAberta *hash = (HashAberta *) malloc(sizeof(HashAberta));
 
     hash->label = label;
     hash->length = length;
     hash->chargeFactor = chargeFactor;
     hash->size = 0;
     hash->node = createNodeSequence(hash->length);
+
+    return hash;
+}
+
+HashAberta *insertHash(HashAberta *hash, Aluno *value) {
+    if ((float) hash->size / (float) hash->length > hash->chargeFactor) {
+        hash = expandsHash(hash);
+    }
+
+    Node *node = hash->node;
+    int index = hashFunction(hash, value->matricula);
+
+    while (index != node->index) {
+        node = node->next;
+    }
+    while (node->status == STATUS_OCUPADO) {
+        node = node->next;
+    }
+
+    node->status = STATUS_OCUPADO;
+    node->value = value;
+    hash->size++;
 
     return hash;
 }
